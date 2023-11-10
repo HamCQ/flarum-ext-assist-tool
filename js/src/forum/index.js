4845740c8deb90d395f3d9e438bed993f00c7234
emin.lin <emin.lin@live.com>
2023-11-10 11:35:21 +0800

    pref:优化指读功能

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,12 +2,12 @@ import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import HeaderSecondary from 'flarum/forum/components/HeaderSecondary';
 import Button from 'flarum/common/components/Button';
-import assistEntry from './assist/assist-entry';
 import LinkButton from 'flarum/common/components/LinkButton';
+import assist from './assist/assist';
 
 app.initializers.add('hamcq/assisttool', () => {
   extend(HeaderSecondary.prototype, 'items', (items) => {
-    assistEntry
+    new assist({"namespace": "hamcq-assist","domain": ""})
     items.add('assist-tool',
       <div 
         class='Button-label' 
